2f8954691c669c2faca108d48b4f2a7eda9935f0
Richard Cox <cox.richard200@gmail.com>
2020-08-20 11:14:32 -0500

    Adding starting files

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# openfaas-action
-A Github Action to build, push, and deploy with OpenFaaS
+<h1 align="center">Welcome to OpenFaaS Github Action (with frills) 👋</h1>
+<p>
+  <a href="https://opensource.org/licenses/MIT" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/RickCoxDev" target="_blank">
+    <img alt="Twitter: RickCoxDev" src="https://img.shields.io/twitter/follow/RickCoxDev.svg?style=social" />
+  </a>
+</p>
+
+> A Github Action to build, push, and deploy using OpenFaaS
+
+## Why?
+
+This action is based on [this](https://github.com/mrsimpson/action-openfaas-build) project. The creator mentioned that they were keeping their action simple and not adding extra parameters. I created this action to have more control over the resulting image's version tag, in order to automate version bumping.
+
+## Usage
+
+
+
+## Author
+
+👤 **Rick Cox**
+
+* Website: https://rickcoxdev.tech
+* Twitter: [@RickCoxDev](https://twitter.com/RickCoxDev)
+* Github: [@RickCoxDev](https://github.com/RickCoxDev)
+* LinkedIn: [@RickCoxDev](https://linkedin.com/in/RickCoxDev)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/RickCoxDev/openfaas-action/issues). You can also take a look at the [contributing guide](https://github.com/RickCoxDev/openfaas-action/issues).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2020 [Rick Cox](https://github.com/RickCoxDev).<br />
+This project is [MIT](https://opensource.org/licenses/MIT) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ entrypoint.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# $1 stack-file
+# $2 docker-username
+# $3 docker-password
+# $4 platforms
+# $5 deploy
+# $6 gateway
+# $7 openfaas-username
+# $8 openfaas-password
+
+# Trigger the shrinkwrap
+BUILDING_MESSAGES_STRING=$(faas-cli build -f $1 --shrinkwrap --tag sha | grep 'Building:')
+SHRINKWRAP_MESSAGES_STRING=$(faas-cli build -f $1 --shrinkwrap --tag sha | grep 'shrink-wrapped')
+
+mapfile -t SHRINKWRAP_MESSAGES <<< "$SHRINKWRAP_MESSAGES_STRING"
+mapfile -t BUILDING_MESSAGES <<< "$BUILDING_MESSAGES_STRING"
+
+for i in "${!BUILDING_MESSAGES[@]}"; do
+    SHRINKWRAP_MESSAGE=${SHRINKWRAP_MESSAGES[i]}
+    BUILDING_MESSAGE=${BUILDING_MESSAGES[i]}
+
+# Parse messages
+    IMAGE_FULL=$(echo $BUILDING_MESSAGE | cut -d' ' -f2)
+    TAG=$(echo $IMAGE_FULL | rev | cut -f1 -d":" | rev) # part after last colon
+    WITHOUT_TAG=$(echo $IMAGE_FULL | sed "s/:$TAG//")
+    IMAGE=$(echo $WITHOUT_TAG | rev | cut -f1 -d"/" | rev) # part after last slash
+    WITHOUT_IMAGE=$(echo $WITHOUT_TAG | sed "s/\/$IMAGE//")
+    ORG=$(echo $WITHOUT_IMAGE| rev | cut -f1 -d"/" | rev) # part after last slash
+
+    SLASH_COUNT=$(tr -dc '/' <<<"$IMAGE_FULL" | awk '{ print length; }')
+    if [ $SLASH_COUNT -gt 1 ]; then
+        REGISTRY=$(echo $WITHOUT_IMAGE | sed "s/\/$ORG//")
+    else
+        REGISTRY=''
+    fi
+    
+# Get mapping to functions and directories
+    FUNCTION=$(echo $SHRINKWRAP_MESSAGE | cut -d' ' -f1)
+    FOLDER=$(echo $SHRINKWRAP_MESSAGE | cut -d' ' -f4)
+    
+    # For debugging purposes
+    echo IMAGE_FULL=$IMAGE_FULL
+    echo REGISTRY=$REGISTRY
+    echo ORG=$ORG
+    echo IMAGE=$IMAGE
+    echo TAG=$TAG
+    echo FUNCTION=$FUNCTION
+    echo FOLDER=$FOLDER
+
+# Authenticate
+    echo $3 | docker login -u $2 --password-stdin $REGISTRY
+
+# Build and push docker image
+    cd "${FOLDER}"
+    export DOCKER_CLI_EXPERIMENTAL=enabled
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+    docker buildx create --use
+    docker buildx install
+    docker build --platform $4 -t $IMAGE_FULL --push .
+    cd -
+done
+
+# TAG is the same across all function builds
+echo ::set-output name=tag::$TAG
+
+# Deploy function stack if requested
+if [ $5 = 'true' ]; then
+echo "Deploying function stack"
+    if [ -z $6 ]; then
+        echo $8 | faas-cli login -u $7 --password-stdin
+        faas-cli deploy -f $1 --image $IMAGE_FULL --tag sha
+    else
+        echo $8 | faas-cli login -u $7 --password-stdin -g $6
+        faas-cli deploy -f $1 --image $IMAGE_FULL --tag sha -g $6
+    fi
+fi
